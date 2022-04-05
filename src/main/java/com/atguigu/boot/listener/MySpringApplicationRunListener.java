ec62434293b885009f3d65fdaf7af62980308793
程序员鱼明 <864224524@qq.com>
2022-04-05 19:44:53 +0800

    third commit

--- src/main/java/com/atguigu/boot/listener/MySpringApplicationRunListener.java
+++ src/main/java/com/atguigu/boot/listener/MySpringApplicationRunListener.java
@@ -15,7 +15,7 @@ public class MySpringApplicationRunListener implements SpringApplicationRunListe
 
     @Override
     public void starting(ConfigurableBootstrapContext bootstrapContext) {
-        System.out.println("MySpringApplicationRunListener....starting....1");
+        System.out.println("MySpringApplicationRunListener....starting....12");
 
     }
 
